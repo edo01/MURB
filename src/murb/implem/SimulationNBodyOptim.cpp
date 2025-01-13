897c4fe935d2f2160a0264d658b40cbfa9370cdd
edo01 <edocarra012@gmail.com>
2025-01-13 18:02:06 +0100

    Reduce the complexity of the algorithm in Optim

--- src/murb/implem/SimulationNBodyOptim.cpp
+++ src/murb/implem/SimulationNBodyOptim.cpp
@@ -26,7 +26,7 @@ void SimulationNBodyOptim::initIteration()
     }
 }
 
-void SimulationNBodyOptim::computeBodiesAcceleration() {
+/*void SimulationNBodyOptim::computeBodiesAcceleration() {
     const std::vector<dataAoS_t<float>> &d = this->getBodies().getDataAoS();
     const unsigned long nBodies = this->getBodies().getN(); 
     const float softSquared = this->soft*this->soft; 
@@ -58,9 +58,53 @@ void SimulationNBodyOptim::computeBodiesAcceleration() {
         this->accelerations[iBody].ay += ay;
         this->accelerations[iBody].az += az;
     }
+}*/
+
+void SimulationNBodyOptim::computeBodiesAcceleration() {
+    const std::vector<dataAoS_t<float>> &d = this->getBodies().getDataAoS();
+    const unsigned long nBodies = this->getBodies().getN(); 
+    const float softSquared = this->soft*this->soft; 
+
+    // flops = n² * 20
+    for (unsigned long iBody = 0; iBody < nBodies; iBody++) {
+        float ax = 0.0f, ay = 0.0f, az = 0.0f; // Local accumulators for acceleration
+
+        const float qx_i = d[iBody].qx;
+        const float qy_i = d[iBody].qy;
+        const float qz_i = d[iBody].qz;
+        const float m_i = d[iBody].m;
+
+        // we skip the diagonal, the interaction of a body with itself is not considered 
+        // and we take advantage of the symmetry of the problem using the third Newton's law
+        for (unsigned long jBody = iBody; jBody < nBodies; jBody++) {
+
+            const float rijx = d[jBody].qx - qx_i;
+            const float rijy = d[jBody].qy - qy_i;
+            const float rijz = d[jBody].qz - qz_i;
+
+            float rijSquared = rijx*rijx + rijy*rijy + rijz*rijz;
+            rijSquared += softSquared;
+            float ai  = this->G/ (rijSquared*std::sqrt(rijSquared));
+            float ai2 = ai;
+            ai = ai * d[jBody].m;
+            ai2 = ai2 * m_i;
+
+            ax += ai * rijx;
+            ay += ai * rijy;
+            az += ai * rijz;
+
+            // we take advantage of the symmetry of the problem using the third Newton's law
+            this->accelerations[jBody].ax -= ai2 * rijx; // CACHE MISS HERE!!!
+            this->accelerations[jBody].ay -= ai2 * rijy;
+            this->accelerations[jBody].az -= ai2 * rijz;
+        }
+
+        // Store the accumulated accelerations
+        this->accelerations[iBody].ax += ax;
+        this->accelerations[iBody].ay += ay;
+        this->accelerations[iBody].az += az;
+    }
 }
-
-
 
 
 void SimulationNBodyOptim::computeOneIteration()
