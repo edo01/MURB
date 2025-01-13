8ef21b8f6200e0458e8347f72970022bb055395c
edo01 <edocarra012@gmail.com>
2025-01-13 16:44:01 +0100

    refactor Optim

--- src/murb/implem/SimulationNBodyOptim.cpp
+++ src/murb/implem/SimulationNBodyOptim.cpp
@@ -29,71 +29,25 @@ void SimulationNBodyOptim::initIteration()
 void SimulationNBodyOptim::computeBodiesAcceleration() {
     const std::vector<dataAoS_t<float>> &d = this->getBodies().getDataAoS();
     const unsigned long nBodies = this->getBodies().getN(); 
-    const float softSquared = std::pow(this->soft, 2); 
+    const float softSquared = this->soft*this->soft; 
 
     // flops = n² * 20
     for (unsigned long iBody = 0; iBody < nBodies; iBody++) {
         float ax = 0.0f, ay = 0.0f, az = 0.0f; // Local accumulators for acceleration
 
-        for (unsigned long jBody = 0; jBody + 3 < nBodies; jBody += 4) { // Unroll by 4
-            // First iteration (jBody)
-            {
-                const float rijx = d[jBody].qx - d[iBody].qx;
-                const float rijy = d[jBody].qy - d[iBody].qy;
-                const float rijz = d[jBody].qz - d[iBody].qz;
+        const float qx_i = d[iBody].qx;
+        const float qy_i = d[iBody].qy;
+        const float qz_i = d[iBody].qz;
 
-                const float rijSquared = std::pow(rijx, 2) + std::pow(rijy, 2) + std::pow(rijz, 2);
-                const float ai = this->G * d[jBody].m / std::pow(rijSquared + softSquared, 3.f / 2.f);
-                ax += ai * rijx;
-                ay += ai * rijy;
-                az += ai * rijz;
-            }
-            // Second iteration (jBody + 1)
-            {
-                const float rijx = d[jBody + 1].qx - d[iBody].qx;
-                const float rijy = d[jBody + 1].qy - d[iBody].qy;
-                const float rijz = d[jBody + 1].qz - d[iBody].qz;
+        for (unsigned long jBody = 0; jBody < nBodies; jBody ++) {
+            const float rijx = d[jBody].qx - qx_i;
+            const float rijy = d[jBody].qy - qy_i;
+            const float rijz = d[jBody].qz - qz_i;
 
-                const float rijSquared = std::pow(rijx, 2) + std::pow(rijy, 2) + std::pow(rijz, 2);
-                const float ai = this->G * d[jBody + 1].m / std::pow(rijSquared + softSquared, 3.f / 2.f);
-                ax += ai * rijx;
-                ay += ai * rijy;
-                az += ai * rijz;
-            }
-            // Third iteration (jBody + 2)
-            {
-                const float rijx = d[jBody + 2].qx - d[iBody].qx;
-                const float rijy = d[jBody + 2].qy - d[iBody].qy;
-                const float rijz = d[jBody + 2].qz - d[iBody].qz;
+            float rijSquared = rijx*rijx + rijy*rijy + rijz*rijz;
+            rijSquared += softSquared;
+            const float ai = this->G * d[jBody].m / (rijSquared*std::sqrt(rijSquared));
 
-                const float rijSquared = std::pow(rijx, 2) + std::pow(rijy, 2) + std::pow(rijz, 2);
-                const float ai = this->G * d[jBody + 2].m / std::pow(rijSquared + softSquared, 3.f / 2.f);
-                ax += ai * rijx;
-                ay += ai * rijy;
-                az += ai * rijz;
-            }
-            // Fourth iteration (jBody + 3)
-            {
-                const float rijx = d[jBody + 3].qx - d[iBody].qx;
-                const float rijy = d[jBody + 3].qy - d[iBody].qy;
-                const float rijz = d[jBody + 3].qz - d[iBody].qz;
-
-                const float rijSquared = std::pow(rijx, 2) + std::pow(rijy, 2) + std::pow(rijz, 2);
-                const float ai = this->G * d[jBody + 3].m / std::pow(rijSquared + softSquared, 3.f / 2.f);
-                ax += ai * rijx;
-                ay += ai * rijy;
-                az += ai * rijz;
-            }
-        }
-
-        // Handle any remaining bodies (if nBodies is not a multiple of 4)
-        for (unsigned long jBody = (nBodies / 4) * 4; jBody < nBodies; jBody++) {
-            const float rijx = d[jBody].qx - d[iBody].qx;
-            const float rijy = d[jBody].qy - d[iBody].qy;
-            const float rijz = d[jBody].qz - d[iBody].qz;
-
-            const float rijSquared = std::pow(rijx, 2) + std::pow(rijy, 2) + std::pow(rijz, 2);
-            const float ai = this->G * d[jBody].m / std::pow(rijSquared + softSquared, 3.f / 2.f);
             ax += ai * rijx;
             ay += ai * rijy;
             az += ai * rijz;
